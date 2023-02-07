3d8db565e09deefb513d74e68e4d7687e61561da
“Vinicius <vinicius.oliveira@fleetcor.com.br>
2023-02-06 21:18:25 -0300

    Styled blocked screen Notification

--- ios/NotificationWidget/NotificationWidget.swift
+++ ios/NotificationWidget/NotificationWidget.swift
@@ -18,36 +18,177 @@ struct NotificationWidgets: WidgetBundle {
     }
 }
 
+struct ProgressBar: View {
+  var width1: CGFloat = 80
+  var width2: CGFloat = 180
+  var width3: CGFloat = 60
+  var heght: CGFloat = 5
+  var percent1: CGFloat = 80
+  var percent2: CGFloat = 180
+  var percent3: CGFloat = 0
+  private var gradientColors = [
+    Color(.green),
+    Color(.black).opacity(0)
+  ]
+  
+  private var gradientColorsCompleted = [
+    Color(.green),
+    Color(.green)
+  ]
+
+  var body: some View {
+    let multiplier1 = width1 / 80
+    let multiplier2 = width2 / 180
+    let multiplier3 = width3 / 60
+
+    VStack {
+      HStack{
+        ZStack(alignment: .leading){
+          RoundedRectangle(cornerRadius: heght, style: .continuous)
+            .frame(width: width1, height: heght)
+            .foregroundColor(Color.black.opacity(0.1))
+
+          RoundedRectangle(cornerRadius: heght)
+            .frame(width: percent1 * multiplier1, height: heght)
+            .background(LinearGradient(gradient: Gradient(colors: gradientColorsCompleted),
+              startPoint: UnitPoint.leading, endPoint: .trailing)
+              .clipShape(RoundedRectangle(cornerRadius: heght, style: .continuous))
+            )
+            .foregroundColor(.clear)
+        }
+        
+        ZStack(alignment: .leading){
+          RoundedRectangle(cornerRadius: heght, style: .continuous)
+            .frame(width: width2, height: heght)
+            .foregroundColor(Color.black.opacity(0.1))
+
+          RoundedRectangle(cornerRadius: heght)
+            .frame(width: percent2 * multiplier2, height: heght)
+            .background(LinearGradient(gradient: Gradient(colors: gradientColors),
+              startPoint: UnitPoint.leading, endPoint: .trailing)
+              .clipShape(RoundedRectangle(cornerRadius: heght, style: .continuous))
+            )
+            .foregroundColor(.clear)
+        }
+        
+        ZStack(alignment: .leading){
+          RoundedRectangle(cornerRadius: heght, style: .continuous)
+            .frame(width: width3, height: heght)
+            .foregroundColor(Color.black.opacity(0.1))
+
+          RoundedRectangle(cornerRadius: heght)
+            .frame(width: percent3 * multiplier3, height: heght)
+            .background(LinearGradient(gradient: Gradient(colors: gradientColors),
+              startPoint: UnitPoint.leading, endPoint: .trailing)
+              .clipShape(RoundedRectangle(cornerRadius: heght, style: .continuous))
+            )
+            .foregroundColor(.clear)
+        }
+      }
+    }
+  }
+}
+
 struct ContetnView: View {
   var body: some View {
     Text("Hello World")
   }
 }
 
+struct ContentNotification: View {
+  var body: some View {
+
+    VStack(alignment: .leading) {
+      HStack(alignment: .center) {
+        Image("logoBurgao")
+          .resizable()
+          .frame( width: 50,height: 50)
+          .background(.white)
+          .cornerRadius(8)
+        
+        VStack(alignment: .leading){
+          Text("Burgão do Zé")
+            .font(.subheadline)
+            .fontWeight(.bold)
+            .foregroundColor(.black)
+            .multilineTextAlignment(.leading)
+          
+          HStack{
+            Text("Pedido:")
+              .font(.subheadline)
+              .fontWeight(.light)
+              .foregroundColor(.gray)
+            Text("#1234")
+              .font(.subheadline)
+              .fontWeight(.bold)
+              .foregroundColor(.gray)
+          }
+        }
+      }
+      
+      Text("Estamos preparando o seu burgão.")
+        .font(.subheadline)
+        .fontWeight(.regular)
+        .foregroundColor(.gray)
+      
+      ProgressBar()
+      
+      Text("Em até 30 minutos seu pedido sai para entrega.")
+        .font(.caption)
+        .fontWeight(.regular)
+        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.755))
+    }
+    .frame(maxWidth: .infinity)
+    .padding(16)
+    .background(.white)
+  }
+}
+
+struct ContentViewLeading: View {
+  var body: some View {
+    VStack(alignment: .trailing) {
+      Image("logoBurgao")
+        .resizable()
+        .frame( width: 26,height: 26)
+        .background(.white)
+        .cornerRadius(30)
+    }
+  }
+}
+
+struct ContentViewTrailing: View {
+  var body: some View {
+    Image(systemName: "clock")
+      .resizable()
+      .frame( width: 22,height: 22)
+      .foregroundColor(.gray)
+  }
+}
+
 struct NotificationActivityWidget: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: NotificationAttributes.self) { context in
-          ContetnView()
+          ContentNotification()
         } dynamicIsland: { context in
           DynamicIsland {
             DynamicIslandExpandedRegion(.leading){
-              ContetnView()
+              Text("Leading")
             }
             DynamicIslandExpandedRegion(.trailing){
-              ContetnView()
+              Text("Trailing")
             }
             DynamicIslandExpandedRegion(.center){
-              ContetnView()
+              Text("Center")
             }
             DynamicIslandExpandedRegion(.bottom){
-              ContetnView()
+              Text("Bottom")
             }
           } compactLeading: {
-            ContetnView()
+            ContentViewLeading()
           } compactTrailing: {
-            ContetnView()
+            ContentViewTrailing()
           } minimal: {
-            ContetnView()
+            ContentViewTrailing()
           }
 
         }
